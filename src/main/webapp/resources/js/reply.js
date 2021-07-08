4f55d615a9e50946fd66ea06ccf84a89e7454e0b
lusida0131 <ks12b9189@naver.com>
2021-07-08 09:42:16 +0900

    reply.js

--- src/main/webapp/resources/js/reply.js
+++ src/main/webapp/resources/js/reply.js
@@ -8,3 +8,10 @@ function updateviewBtn(r_num, r_content, id) {
 	commentsview += '작성자 : ' + id;
 	commentsview += '</strong>&nbsp;&nbsp;&nbsp;';
 	commentsview += '댓글 내용 : &nbsp;&nbsp;&nbsp;';
+	commentsview +='<textarea class="form-control" id="reply_edit_content">';
+	commentsview += r_content;
+	commentsview += '</textarea>';
+	commentsview += '</p>';
+	commentsview += '<br/>';
+	commentsview += <button type="button" class="btn-outline-success"';
+	commentsview += 'onclick="up
